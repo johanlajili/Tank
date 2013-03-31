135c270371b4ddf293e7bc149ffcf1094b0a2068
Web Server <anthony.pigeot@gmail.com>
2013-03-31 13:50:18 +0200

    server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var server	= require('http').createServer(app);
 var io = require('socket.io').listen(server);
 io.set('log level', 1);
-server.listen(1337);
+server.listen(4242);
 
 //app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 
 	res.render('home.jade');
 });
-app.get('/game', function(req, res){
+app.get('/tank', function(req, res){
 
 	res.render('game.jade');
 });
